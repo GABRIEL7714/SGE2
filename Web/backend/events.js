ff0150b93f59017b14c2555a2c2f071a75c1ad0b
WalterJoel <walter.valdivia@ucsp.edu.pe>
2024-10-24 12:24:29 -0500

    [fix] Header and Footer from index.html

--- Web/backend/events.js
+++ Web/backend/events.js
@@ -1,36 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const events = [
-        {
-            title: 'Evento 1',
-            date: '05/07/24',
-            img: 'https://via.placeholder.com/150',
-            fechaInscripcion: "2025-10-12",
-        },
-        {
-            title: 'Evento 2',
-            date: '12/10/25',
-            img: 'https://via.placeholder.com/150',
-        },
-        {
-            title: 'Evento 3',
-            date: '12/10/25',
-            img: 'https://via.placeholder.com/150',
-        },
-        {
-            title: 'Evento 4',
-            date: '12/10/25',
-            img: 'https://via.placeholder.com/150',
-        }
-    ];
+  const events = [
+    {
+      title: 'Evento 1',
+      date: '05/07/24',
+      // img: 'https://via.placeholder.com/150',
+      fechaInscripcion: '2025-10-12',
+    },
+    {
+      title: 'Evento 2',
+      date: '12/10/25',
+      // img: 'https://via.placeholder.com/150',
+    },
+    {
+      title: 'Evento 3',
+      date: '12/10/25',
+      img: 'https://via.placeholder.com/150',
+    },
+    {
+      title: 'Evento 4',
+      date: '12/10/25',
+      img: 'https://via.placeholder.com/150',
+    },
+  ];
 
-    const eventContainer = document.getElementById('eventContainer');
+  const eventContainer = document.getElementById('eventContainer');
 
-    events.forEach(event => {
-        // Crear la tarjeta del evento
-        const eventCard = document.createElement('div');
-        eventCard.classList.add('col-md-4', 'mb-4');
+  events.forEach((event) => {
+    // Crear la tarjeta del evento
+    const eventCard = document.createElement('div');
+    eventCard.classList.add('col-md-4', 'mb-4');
 
-        eventCard.innerHTML = `
+    eventCard.innerHTML = `
            <div class="card h-100">
                 <img src="${event.img}" class="card-img-top" alt="${event.title}">
                 <div class="card-body text-center">
@@ -41,13 +41,14 @@ document.addEventListener('DOMContentLoaded', function () {
             </div>
         `;
 
-        eventContainer.appendChild(eventCard);
-    });
+    eventContainer.appendChild(eventCard);
+  });
 });
-
 
 // Función para redirigir a la página de combos con la información del evento seleccionado
 function verCombos(eventTitle, fechaInscripcion) {
-    // Redirigir a combos.html pasando la información del evento como parámetros en la URL
-    window.location.href = `combos.html?title=${encodeURIComponent(eventTitle)}&fechaInscripcion=${encodeURIComponent(fechaInscripcion)}`;
+  // Redirigir a combos.html pasando la información del evento como parámetros en la URL
+  window.location.href = `combos.html?title=${encodeURIComponent(
+    eventTitle
+  )}&fechaInscripcion=${encodeURIComponent(fechaInscripcion)}`;
 }
