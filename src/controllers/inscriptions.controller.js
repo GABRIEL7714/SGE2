8f95ca1454c9a71625ecd93d127273f12671fedb
Walter Gabriel Valdivia Bejarano <101744040+GABRIEL7714@users.noreply.github.com>
2024-11-02 20:51:43 -0500

    Update inscriptions.controller.js

--- src/controllers/inscriptions.controller.js
+++ src/controllers/inscriptions.controller.js
@@ -1,42 +1,18 @@
 // import { events, users } from '../mockdb/mockdb.js';
 
 // //Genero recibo del evento
+//IC-001
 // const createInvoice = async () => {
 //   const message = 'Se ha enviado el comprobante electronico a su correo';
 //   return message;
 // };
 
+//IC-002
 // const findUser = async (userId) => {
 //   return users.find((user) => user.id === userId);
 // };
+
+//IC-003
 // const findEvent = async (eventId) => {
 //   return events.find((event) => event.id === eventId);
-// };
-
-// //* Gabo de esta forma envias por parametros
-// export const registerToEvent = async (req, res) => {
-//   const userId = parseInt(req.params.userId);
-//   const eventId = parseInt(req.params.eventId);
-
-//   const user = await findUser(userId);
-//   const event = await findEvent(eventId);
-
-//   const invoiceMessage = await createInvoice();
-
-//   const message =
-//     'Usted ha sigo registrado para el evento: ' +
-//     event.eventName +
-//     ' --- ' +
-//     user.email +
-//     '---->' +
-//     invoiceMessage;
-
-//   return res.json(message);
-// };
-
-// //* Gabo de esta forma envias por el cuerpo (body)
-// // en insomnia pones primero pones POST y luego entre llaves { email: " de email", password: "de password"}
-// export const envioBody = async (req, res) => {
-//   const { email, password } = req.body;
-//   return res.json(email);
 // };
